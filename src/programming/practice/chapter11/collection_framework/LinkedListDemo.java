de8a6b3b51c7b43f55aaf914471fe2d770598c81
Md. Tanvir Hossain <tanvir15-7157@diu.edu.bd>
2019-07-15 17:04:31 +0600

    chapter-11 added

---
+++ src/programming/practice/chapter11/collection_framework/LinkedListDemo.java
@@ -0,0 +1,43 @@
+package programming.practice.chapter11.collection_framework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		List <String> emperors = new ArrayList<>();
+		
+		emperors.add("Tanvir");
+		emperors.add("Hossain");
+		emperors.add("rasel");
+		
+		System.out.println("Name of emperors: " + emperors);
+		System.out.println("Emperors size: " + emperors.size());
+		
+		//getting second emperors name
+		String secondEmperor = emperors.get(1);
+		System.out.println("2nd Emperors: " + secondEmperor);
+		
+		emperors.set(0, "Tanvir the lazy");
+		System.out.println("Name of emperors: " + emperors);
+		
+		emperors.remove("rasel");
+		System.out.println("Afer removing, Name of emperors: " + emperors);
+		
+		emperors.add("ribon");
+		System.out.println("After adding, Name of emperors: " + emperors);
+
+		//add duplicate
+		emperors.add("ribon");
+		System.out.println("after: Name of emperors: " + emperors);
+		
+		int indexOfHossain = emperors.indexOf("Hossain");
+        System.out.println("Index of Hossain :" + indexOfHossain);
+        
+        int lastIndexOfRibon = emperors.lastIndexOf("ribon");
+        System.out.println("Last index of ribon: "+ lastIndexOfRibon);
+	}
+
+}
---
+++ src/programming/practice/chapter8/generics/Chapter8.java
@@ -0,0 +1,10 @@
+package programming.practice.chapter8.generics;
+
+public class Chapter8 {
+
+	public static void main(String[] args) {
+		System.out.println("Chaptr-8");
+
+	}
+
+}
