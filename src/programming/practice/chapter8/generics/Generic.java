be9f0e1ebb2cdfc9a88e9eecfc07b3179eadf337
Md. Tanvir Hossain <mdttanvirhossain@gmail.com>
2020-01-23 01:12:08 +0600

    chp-8 update

---
+++ src/programming/practice/chapter8/generics/Generic.java
@@ -0,0 +1,25 @@
+package programming.practice.chapter8.generics;
+
+public class Generic<T> {
+
+	private T obj;
+
+	public Generic(T obj) {
+		//super();
+		this.obj = obj;
+	}
+
+	public T getObj() {
+		return obj;
+	}
+
+	public void setObj(T obj) {
+		this.obj = obj;
+	}
+	
+	public void showType() {
+		System.out.println("Type of T is: " +obj.getClass().getName());
+	}
+	
+	
+}
